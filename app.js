15fae2f5d90beb79110e419d8f4591bd5535c6a1
Pierce <piercepatrick2001@gmail.com>
2021-02-22 21:51:32 -0600

    deleted grid code because it needs to be rewritten to where it will size within the container, added font family

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const gridContainer = document.getElementById("gridContainer");
 
-function makeRows(rows, cols) {
-    gridContainer.style.setProperty('--grid-rows', rows);
-    gridContainer.style.setProperty('--grid-cols', cols);
-    for (c = 0; c < (rows * cols); c++) {
-      let cell = document.createElement("div");
-      cell.innerText = ' ';
-      gridContainer.appendChild(cell).className = "grid-item";
-    };
-  };
-  
-  makeRows(10, 10);
+window.addEventListener('load', makeRows);
+function makeRows(rows, cols) 
+{
+ 
+}
+
+function changeSize() 
+{
+
+}
+
+function changeColor() 
+{
+
+}
+
+function resetGame()
+
 
 
