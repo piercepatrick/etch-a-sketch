12ebbe83ff7ee27d9754586bdc4d913259f12c17
Pierce <piercepatrick2001@gmail.com>
2021-02-23 15:42:16 -0600

    added grid

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const gridContainer = document.getElementById("gridContainer");
 
-window.addEventListener('load', makeRows);
-function makeRows(rows, cols) 
+let size = 16;
+
+window.addEventListener('load', makeRows(size));
+
+function makeRows(size) 
 {
- 
+    gridContainer.setAttribute('style','grid-template-columns:repeat('+size+',1fr);grid-template-rows:repeat('+size+',1fr);');
+    for (i = 0; i < size*size; i++)
+    {
+        const div = document.createElement('div');
+        div.className = 'gridItem';
+        div.style.border = '1px solid rgb(0, 0, 0)';
+        gridContainer.appendChild(div);
+    }
+
 }
 
 function changeSize() 
@@ -17,6 +28,6 @@ function changeColor()
 }
 
 function resetGame()
+{
 
-
-
+}
