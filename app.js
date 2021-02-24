078392af07ef568dd56dd4f0ccc0d8ea6abc9b40
Pierce <piercepatrick2001@gmail.com>
2021-02-23 22:27:27 -0600

    reset grid function works

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 const gridContainer = document.getElementById("gridContainer");
+const sizeButton = document.getElementById("sizeButton");
+const resetButton = document.getElementById("resetButton");
+
 
 let size = 16;
 
 window.addEventListener('load', makeRows(size));
+sizeButton.addEventListener('click', changeSize);
+resetButton.addEventListener('click', resetGame);
 
 function makeRows(size) 
 {
     gridContainer.setAttribute('style','grid-template-columns:repeat('+size+',1fr);grid-template-rows:repeat('+size+',1fr);');
     for (i = 0; i < size*size; i++)
     {
-        let square = document.createElement('div');
+        const square = document.createElement('div');
         square.className = 'gridItem';
         square.style.border = '1px solid rgb(0, 0, 0)';
         gridContainer.appendChild(square);
@@ -32,6 +37,10 @@ function changeColor(box)
 }
 
 function resetGame()
-{
-
+{ 
+    let square = document.getElementsByClassName("gridItem");
+    for (var i = 0; i < square.length; i++) {
+        square[i].style.backgroundColor = 'rgb(255,255,255)';
+    }
+    
 }
