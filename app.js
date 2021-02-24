1aec803d315843126eb926a374d4e7f159d923a5
Pierce <piercepatrick2001@gmail.com>
2021-02-23 23:09:04 -0600

    fixed issue where grid items still existed when you go to change size of grid

--- app.js
+++ app.js
@@ -2,21 +2,19 @@ const gridContainer = document.getElementById("gridContainer");
 const sizeButton = document.getElementById("sizeButton");
 const resetButton = document.getElementById("resetButton");
 
-
+resetButton.addEventListener('click', () => location.reload());
 let size = 16;
-
 window.addEventListener('load', makeRows(size));
 sizeButton.addEventListener('click', changeSize);
 resetButton.addEventListener('click', resetGame);
 
-function makeRows(size) 
+function makeRows() 
 {
     gridContainer.setAttribute('style','grid-template-columns:repeat('+size+',1fr);grid-template-rows:repeat('+size+',1fr);');
     for (i = 0; i < size*size; i++)
     {
-        const square = document.createElement('div');
+        let square = document.createElement('div');
         square.className = 'gridItem';
-        square.style.border = '1px solid rgb(0, 0, 0)';
         gridContainer.appendChild(square);
         square.addEventListener('mouseover', changeColor);
     }
@@ -25,7 +23,17 @@ function makeRows(size)
 
 function changeSize() 
 {
-
+    let newGrid = prompt("Please Enter a New Size For Your Grid. 1-64");
+    if (newGrid >= 1 && newGrid <= 64)
+    {
+        clearGrid();
+        size = newGrid;
+        makeRows();
+    }
+    else
+    {
+        alert('Enter a Number from 1-64!!');
+    }
 }
 
 function changeColor(box) 
@@ -36,11 +44,10 @@ function changeColor(box)
     box.target.style.background = `rgb(${Color1}, ${Color2}, ${Color3})`;
 }
 
-function resetGame()
-{ 
-    let square = document.getElementsByClassName("gridItem");
-    for (var i = 0; i < square.length; i++) {
-        square[i].style.backgroundColor = 'rgb(255,255,255)';
-    }
-    
+function clearGrid() 
+{
+    const gridArray = Array.from(gridContainer.childNodes);
+    gridArray.forEach((element) => {
+        gridContainer.removeChild(element);
+    });
 }
